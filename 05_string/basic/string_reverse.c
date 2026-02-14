a34113751002a55c2ea92774a8424cf226e46578
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-14 10:39:31 +0545

    find str reverse

---
+++ 05_string/basic/string_reverse.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int stringLength(char *str)
+{
+    int len = 0;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+        len++;
+    }
+    return len - 1;
+}
+
+void stringRev(char *str)
+{
+    int i = 0, j = stringLength(str) - 1;
+    while (i < j)
+    {
+        int t = str[i];
+        str[i] = str[j];
+        str[j] = t;
+        i++;
+        j--;
+    }
+}
+
+int main()
+{
+    char str[50];
+    printf("Enter a string: ");
+    fgets(str, (sizeof str), stdin);
+
+    stringRev(str);
+    printf("Reversed string: %s", str);
+
+    return 0;
+}

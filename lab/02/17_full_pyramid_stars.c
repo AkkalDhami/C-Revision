a52735ece33c2f932b66ccb45cca1a40c2630422
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-26 11:53:26 +0545

    Full Pyramid of Stars

---
+++ lab/02/17_full_pyramid_stars.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//? Full Pyramid of Stars
+//? Spaces = N - i
+//?  Stars = 2 * i - 1
+
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int s = 1; s <= n - i; s++)
+            printf(" ");
+
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}

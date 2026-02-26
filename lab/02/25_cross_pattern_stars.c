83e54dd2fa86050c55db6f898da2a661e4020131
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-26 11:54:48 +0545

    cross pattern using stars

---
+++ lab/02/25_cross_pattern_stars.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//? Write a C program to print the following cross pattern using stars.
+int main()
+{
+    int n;
+
+    printf("Enter size (N): ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == j || i + j == n + 1)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

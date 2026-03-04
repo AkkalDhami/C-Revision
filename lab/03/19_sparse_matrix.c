2282286ebeebc3c0292fbb525811cf64f57081d1
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-04 20:02:50 +0545

    heck whether a given matrix is a sparse matrix

---
+++ lab/03/19_sparse_matrix.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* 19. Write a program to check whether a given matrix is a sparse matrix(more zeros than non-zeros). */
+int main() {
+    int r, c, zeros = 0;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+    int m[r][c];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &m[i][j]);
+            if (m[i][j] == 0) zeros++;
+        }
+    }
+    if (zeros > (r * c) / 2) printf("It is a sparse matrix.\n");
+    else printf("It is not a sparse matrix.\n");
+    return 0;
+}

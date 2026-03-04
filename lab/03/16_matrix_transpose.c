d2cb6945966982763afb05b33c2ff76ad048ca6d
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-04 20:01:47 +0545

    find the transpose of a given matrix

---
+++ lab/03/16_matrix_transpose.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* 16. Write a program to find the transpose of a given matrix. */
+int main() {
+    int r, c;
+    printf("Enter rows and cols: ");
+    scanf("%d %d", &r, &c);
+    int m[r][c];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+    printf("Transpose:\n");
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            printf("%d\t", m[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
